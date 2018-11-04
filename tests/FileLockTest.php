a4567422538d85b584ec86e041001d59edc19164
ivancoff <44445575+ivancoff@users.noreply.github.com>
2018-11-04 12:10:48 +0300

    Add files via upload

--- tests/FileLockTest.php
+++ tests/FileLockTest.php
@@ -2,51 +2,67 @@
 
 use PHPUnit\Framework\TestCase;
 
-$fname = 'tests/lock-test.txt';
-
-//create file
-$handle = fopen($fname, 'w') or die('ERROR[0]: Unable to open/create file "'.$fname.'"!');
-$txt = date('d.m.Y H:i:s');
-fwrite($handle, $txt);
-fclose($handle); 
-echo PHP_EOL .'Test file "'.$fname.'" created ['.date('d.m.Y H:i:s').']. FileSize: '.filesize($fname).' bytes.'. PHP_EOL . PHP_EOL; 
-
-
 class FileLockTest extends TestCase {
 
-	public function testCanBeLocked() {
+	private $fname;
+	
+    protected function setUp() 
+	{
+        parent::setUp();
+        $this->fname = 'tests/lock-test.txt';
+    }
 
-		global $fname;
-		//create lock object
-		$lock = new \Dorantor\FileLock($fname);
+    public function testInvalidLockId() 
+	{
+        //try to catch exception from Class if the file name is incorrect
+		$f = new \Dorantor\FileLock('');
+		self::assertFalse($f->acquire());
+    }
+	
+	public function testLock()
+    {
+        $lock = new \Dorantor\FileLock($this->fname);
+
+        //try to lock file
+		self::assertTrue($lock->acquire(),'ERROR[0]: Result of function acquire() not correct!'. PHP_EOL);
+        return $lock;
 		
-		//try to lock file
-        $this->assertEquals(true, $lock->acquire(),'ERROR[1]: Result of function acquire() not correct!'. PHP_EOL);
-/*		
-		//try to  read write
-		$handle = fopen($fname, 'r+');
-		$this->assertTrue(($handle != false),'ERROR[2]: file "'.$fname.'" can not be opened!'. PHP_EOL);
-		
-		//try to read
-		//$buffer = fgets($handle, 4096);
-		//$this->assertFalse($buffer,'ERROR[3]: file "'.$fname.'"  not locked! File content: '.$buffer. PHP_EOL);
-		
-		//try to write
-		$fwrite_res = fwrite($handle, 'test');
-		$this->assertEquals(0, $fwrite_res,'ERROR[5]: writed '.$fwrite_res.' bytes.'. PHP_EOL);
-		
-		fclose($handle);
-*/
+    }
+
+    /**
+     * @depends     testLock
+     * @param       Dorantor\FileLock       $lock
+     */
+    public function testLockLockedFile()
+    {
+		//try to lock locked file - wait false result
+		$f = new \Dorantor\FileLock($this->fname);
+		self::assertFalse($f->acquire());
+    }	
+
+    /**
+     * @depends     testLock
+     * @param       Dorantor\FileLock       $lock
+     */
+    public function testUnlock(Dorantor\FileLock $lock)
+    {
 		//try to unlock file
-		$this->assertEquals(true, $lock->release(),'ERROR[6]: Result of function release() not correct!'. PHP_EOL);
-/*		
-		$handle = fopen($fname, 'r+');	
+		self::assertTrue($lock->release(),'ERROR[2]: Result of function release() not correct!'. PHP_EOL);
+    }
+	
+	public function testFileUnLocked()
+    {
+		//try to read/write file after UnLocking
+		$handle = fopen($this->fname, 'a+');
+		self::assertTrue(($handle !== false),'ERROR[3]: file "'.$this->fname.'" can not be opened!'. PHP_EOL);		
 
 		//try to write
-		$fwrite_res = (fwrite($handle, 'test') != 0);
-		$this->assertTrue($fwrite_res,'ERROR[7]: File "'.$fname.'" locked! Writing fail.'. PHP_EOL);
-		   
-		fclose($handle);
-*/
+		$fwrite_res = fwrite($handle, ''.date('d.m.Y H:i:s'). PHP_EOL);
+		self::assertNotEquals(0, $fwrite_res,'ERROR[4]: can not write to file '.$this->fname.''. PHP_EOL);
+		
+		fclose($handle);		
+		
+		$read_res = file_get_contents($this->fname, FALSE, NULL, 0, 1024);
+		self::assertNotEquals(0, strlen($read_res),'ERROR[5]: file still locked'. PHP_EOL);
 	}
 }
